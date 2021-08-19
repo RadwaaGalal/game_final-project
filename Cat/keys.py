3ee18f0f12f9dba5e98c378a8a74f2ddb2d64fa6
Mahmoud Hamdy <50111950+MahmoudHamdyMahmoud@users.noreply.github.com>
2021-08-19 02:26:57 +0200

    update

--- Cat/keys.py
+++ Cat/keys.py
@@ -1,9 +1,6 @@
 import pygame
 import time
 import random
-import pygame
-import time
-import random
 YELLOW = (255, 228, 196)
 BLUE = (30, 144, 255)
 GREEN = (0, 100, 0)
@@ -64,10 +61,10 @@ def background_green():
 def game_over():
     time.sleep(0.2)
     win.fill(BLACK)
-    Game_over = pygame.image.load('Image/over.png')
+    Game_over = pygame.image.load('over.png')
     win.blit(Game_over, (50, 100))
     time.sleep(1)
-    sound_obj = pygame.mixer.Sound('Image/Game_over.mp3')
+    sound_obj = pygame.mixer.Sound('Game_over.mp3')
     sound_obj.play()
     time.sleep(4)
     sound_obj.stop()
@@ -78,25 +75,24 @@ while not exit:
     pygame.draw.rect(win, BLACK, [102, 52, 718, 718])
     background_blue()
     background_green()
-    image = pygame.image.load('Image/cat2.png')
+    image = pygame.image.load('cat2.png')
     win.blit(image, (cat_randomx, cat_randomy))
-    image2 = pygame.image.load('Image/mouse.png')
+    image2 = pygame.image.load('mouse.png')
     win.blit(image2, (mouse_randomx, mouse_randomy))
-
     if (mouse_randomx < 250 and (mouse_randomy > 200 and mouse_randomy < 318)) or mouse_randomx > 630 or mouse_randomy < 200 or mouse_randomy > 570:
         game_over()
         exit = True
     if (mouse_randomx < 250 and mouse_randomy > 420):
         game_over()
         exit = True
-    if mouse_random == cat_random:
+    if cat_random == mouse_random:
         game_over()
         exit = True
     elif mouse_randomx < 200 and (mouse_randomy < 420 and mouse_randomy > 300):
         win.fill(BLACK)
-        wins = pygame.image.load('Image/wins.png')
+        wins = pygame.image.load('wins.png')
         win.blit(wins, (50, 100))
-        sound_obj = pygame.mixer.Sound('Image/winning.mp3')
+        sound_obj = pygame.mixer.Sound('winning.mp3')
         sound_obj.play()
         time.sleep(2)
         sound_obj.stop()
